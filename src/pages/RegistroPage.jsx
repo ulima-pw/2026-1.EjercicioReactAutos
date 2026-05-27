6f19882ada092caebeb9eb8ad6a838d18b1bac1f
ProfA1602 <ProfA1602@lab.aca>
2026-05-27 11:49:21 -0500

    Formulario de registro sin estilos

--- src/pages/RegistroPage.jsx
+++ src/pages/RegistroPage.jsx
@@ -1,5 +1,21 @@
 function RegistroPage() {
-    return <div>Registro Page</div>
+    return <>
+        <h1 className="font-black text-4xl mb-4">Registro de Auto</h1>
+        <form>
+            <div>
+                <label>Marca</label>
+                <input className="border border-gray-200" type="text" />
+            </div>
+            <div>
+                <label>Modelo</label>
+                <input className="border border-gray-200" type="text" />
+            </div>
+            <div>
+                <label>Placa</label>
+                <input className="border border-gray-200" type="text" />
+            </div>
+        </form>
+    </>
 }
 
 export default RegistroPage
